c2ca30e5b697a3e5c920bf6e137ff2c6ed1d3992
Aleksandr Pasechnik <al@megamicron.net>
2025-10-23 00:17:27 -0400

    no more startup screen

--- emacs-custom-popos.el
+++ emacs-custom-popos.el
@@ -5,9 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wheatgrass))
+ '(inhibit-startup-screen t)
  '(org-hide-emphasis-markers t)
- '(package-selected-packages
-   '(dumb-jump forge magit markdown-mode which-key)))
+ '(package-selected-packages '(dumb-jump forge magit markdown-mode which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
