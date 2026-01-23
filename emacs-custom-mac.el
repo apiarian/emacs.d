66853f05099303b7f9a2eb2028f73f952817ef78
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2026-01-23 12:15:39 -0500

    a little bigger font

--- emacs-custom-mac.el
+++ emacs-custom-mac.el
@@ -30,4 +30,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 130 :width normal :foundry "nil" :family "VictorMono Nerd Font Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "VictorMono Nerd Font Mono")))))
