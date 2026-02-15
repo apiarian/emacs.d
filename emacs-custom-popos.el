181dfad37a965bf98ed8ec58026ad3356a0b6698
Aleksandr Pasechnik <al@megamicron.net>
2026-02-15 17:21:10 -0500

    package shuffles

--- emacs-custom-popos.el
+++ emacs-custom-popos.el
@@ -6,10 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(org-hide-emphasis-markers t)
- '(package-selected-packages
-   '(agent-shell avy dockerfile-mode dumb-jump forge god-mode helm-org
-		 helm-slime highlight-thing paredit-menu
-		 solarized-theme typescript-mode yaml-mode))
+ '(package-selected-packages '(shell-maker))
  '(tab-bar-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
